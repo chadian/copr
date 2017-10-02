2a7c0210cf53de01e8dca193b2d852565e732319
Chad Carbert <chadcarbert@me.com>
2017-10-03 00:49:18 +0200

    Add stubs and helpers

---
+++ spec/stubs/player.js
@@ -0,0 +1,11 @@
+const Board = require('../../board');
+const Player = require('../../player');
+
+const X = 'X';
+const O = 'O';
+
+exports._ = Board.EMPTY_SPOT_SYMBOL;
+exports.X = X;
+exports.O = 'O';
+exports.playerX = new Player(X);
+exports.playerO = new Player(O);
---
+++ spec/stubs/position.js
@@ -0,0 +1,10 @@
+// english helpers corresponding to array indexes
+exports.TOP_LEFT      = 0;
+exports.TOP_MIDDLE    = 1;
+exports.TOP_RIGHT     = 2;
+exports.MIDDLE_LEFT   = 3;
+exports.MIDDLE_MIDDLE = 4;
+exports.MIDDLE_RIGHT  = 5;
+exports.BOTTOM_LEFT   = 6;
+exports.BOTTOM_MIDDLE = 7;
+exports.BOTTOM_RIGHT  = 8;
