23f489ae9edafaa772cf0bdd2e34d3464a8c56b8
Chad Carbert <chadcarbert@me.com>
2017-10-03 18:43:58 +0200

    Clean up winner-of-board and check for appropriate final winner

--- utils/winner-of-board.js
+++ utils/winner-of-board.js
@@ -1,28 +1,31 @@
 const Board = require('../board');
+const { uniq } = require('ramda');
 
 function winnerOfBoard(board) {
-  const winners = Board.LINES.map(line => {
-    const symbol = board.symbolAtSpot(line[0]);
+  let winners = Board.LINES
+    .map(line => {
+      const symbol = board.symbolAtSpot(line[0]);
 
-    if (symbol === Board.EMPTY_SPOT_SYMBOL) {
-      return null;
-    }
+      if (symbol === Board.EMPTY_SPOT_SYMBOL) {
+        return null;
+      }
 
-    const isWinner = line.every(
-      spot => board.symbolAtSpot(spot) === symbol
-    );
+      const isWinner = line.every(
+        spot => board.symbolAtSpot(spot) === symbol
+      );
 
-    return isWinner ? symbol : null;
-  })
-  .filter(winner => winner !== null);
+      return isWinner ? symbol : null;
+    })
+    .filter(winner => winner !== null);
 
-  if (winners.length > 1) {
-    console.log(winners);
-    throw 'should not be more than winner on the board';
-  }
+  winners = uniq(winners);
 
   if (winners.length === 0) {
     return null;
+  }
+
+  if (winners.length > 1) {
+    throw 'should not be more than one winner on the board';
   }
 
   return winners[0];
