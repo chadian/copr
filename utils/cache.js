4f5a07170ad293e00b601b8247f6abbc82d70e60
Chad Carbert <chadcarbert@me.com>
2017-11-18 22:22:08 +0100

    Add cache memoization options for board

---
+++ utils/cache.js
@@ -0,0 +1,26 @@
+const { memoizeWith, nAry } = require('ramda');
+
+const cacheKeyGenerator = (board, playerSymbol, opponentSymbol) => {
+  const PLAYER_CACHE_CHAR = 'X';
+  const OPPONENT_CACHE_CHAR = 'O';
+  const cacheKey = board
+    .toArray()
+    .map(symbol => {
+      if (symbol ===  playerSymbol) return PLAYER_CACHE_CHAR;
+      if (symbol === opponentSymbol) return OPPONENT_CACHE_CHAR;
+      else return symbol;
+    })
+    .join(',');
+
+  return cacheKey;
+};
+
+const memoizeBoardWithPlayer = fn => memoizeWith(
+  (board, playerA, playerB) => cacheKeyGenerator(board, playerA.symbol, playerB.symbol),
+  fn
+);
+
+module.exports = {
+  cacheKeyGenerator,
+  memoizeBoardWithPlayer
+};
