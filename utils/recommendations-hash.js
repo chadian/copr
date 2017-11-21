b966f648ed4188ff84e47f53829e1302441c7f40
Chad Carbert <chadcarbert@me.com>
2017-11-21 08:30:10 +0100

    Add recommendations hash

---
+++ utils/recommendations-hash.js
@@ -0,0 +1,27 @@
+const buildMoveTree = require('./build-move-tree');
+
+module.exports = function recommendationHash(board, playerX, playerO) {
+  const hash = {};
+  const moveTree = buildMoveTree(board, playerO, playerX);
+
+  const moveForNode = (node) => {
+    const board = node.board;
+    const hashString = board.toArray().toString();
+
+    // hash already exists, no work to be done
+    if (hash[hashString]) return;
+
+    let bestMove;
+    try {
+      bestMove = recommendBestMove(board, playerX, playerO);
+    } catch (exception) {
+      bestMove = null;
+    }
+
+    hash[hashString] = bestMove;
+    node.children.forEach(node => moveForNode(node));
+  }
+
+  moveForNode(moveTree);
+  return hash;
+};
