8186c015ec850efd5e82281ee29d26dd7573a10f
Chad Carbert <chadcarbert@me.com>
2017-12-17 03:28:26 +0100

    Reorder recommendations-hash conditions
    - Recursively call children early to avoid early guards escaping early and ending
    recursive chain.
    - Determine win and draw states earlier
    - Handle empty board explicitly with no recommendation

--- utils/recommendations-hash.js
+++ utils/recommendations-hash.js
@@ -9,35 +9,42 @@ module.exports = function recommendationHash(board, player, opponent) {
 
   const moveForNode = (node) => {
     const board = node.board;
+
+    // recursive early to determine best moves for children
+    node.children.forEach(node => moveForNode(node));
+
     const hashString = board.toArray().toString();
 
     // hash already exists, no work to be done
     if (hash[hashString]) return;
 
-    node.children.forEach(node => moveForNode(node));
+    const totalMovesMade = board.toArray().filter(
+      spot => spot === player.symbol || spot === opponent.symbol
+    ).length;
+
+
+    if (winnerOfBoard(board)) {
+      hash[hashString] = 'WIN';
+    } else if(totalMovesMade === board.toArray().length) {
+      // the board is completely full
+      hash[hashString] = 'DRAW';
+    }
+
+    if (totalMovesMade % 2 === 0 || totalMovesMade === 0) {
+      // only provide recommendations on moves where the opponent
+      // has already completed their move.
+      return;
+    }
 
     let bestMove;
     try {
       bestMove = recommendBestMove(board, opponent, player);
     } catch (exception) {
-      bestMove = null;
-    }
-
-    const totalMovesMade = board.toArray().filter(
-      spot => spot === player.symbol || spot === opponent.symbol
-    ).length;
-
-    if (winnerOfBoard(board)) {
-      hash[hashString] = 'WIN';
-    } else if (totalMovesMade % 2 === 0) {
-      // only provide recommendations on moves where the opponent
-      // has already completed their move.
+      // skip when unable to determine the best move
       return;
-    } else if (typeof bestMove === 'number') {
-      hash[hashString] = bestMove;
-    } else {
-      hash[hashString] = 'DRAW';
     }
+
+    hash[hashString] = bestMove;
   }
 
   moveForNode(moveTree);
